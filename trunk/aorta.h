939632259e250c829ee8dcbe15a5e9d53e40c6b2
ghs <ghs@fa0eb41c-acb6-4dfe-9f06-9ea70920c729>
2006-05-14 13:37:06 +0000

    initial commit

---
+++ trunk/aorta.h
@@ -0,0 +1,116 @@
+/*
+ 
+ aorta.h: the Aleph One Replacement Texture Utility
+ Copyright (C) 2006  Gregory Smith
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ 
+ */
+
+#ifndef AORTA_H
+#define AORTA_H
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP 
+#include <wx/wx.h>
+#include <wx/image.h>
+#include <wx/notebook.h>
+#include <wx/sizer.h>
+#include <wx/statbmp.h>
+#include <wx/statline.h>
+
+#include <wx/bitmap.h>
+#include <wx/bmpbuttn.h>
+#endif
+
+#include "image_ext.h"
+
+class MainApp: public wxApp // MainApp is the class for our application 
+{
+	// MainApp just acts as a container for the window, 
+public: 
+	// or frame in MainFrame 
+	virtual bool OnInit(); 
+};
+
+class MainFrame: public wxFrame // MainFrame is the class for our window, 
+{ 
+	// It contains the window and all objects in it 
+public: 
+	MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size, long style); 
+	wxNotebook *notebook;
+	void OnExit(wxCommandEvent& event);
+
+}; 
+
+class BasicPage: public wxNotebookPage
+{
+public: 
+	BasicPage(wxWindow* parent, wxWindowID id, const wxPoint &pos, const wxSize &size);
+	
+	void OnLoadNormal(wxCommandEvent &);
+	void OnLoadMask(wxCommandEvent &);
+	void OnClearMask(wxCommandEvent &);
+	void OnOpacTypeTwo(wxCommandEvent &);
+	void OnOpacTypeThree(wxCommandEvent &);
+	void OnSaveAs(wxCommandEvent &);
+	
+	void UpdateNormalDisplay();
+	void UpdateMaskDisplay();
+	
+	void SetMaskButtonEnablement(bool enabled);
+	
+	DECLARE_EVENT_TABLE()
+		
+private:
+	wxImageExt normalImage;
+	wxImageExt normalImageDisplay;
+	wxButton *normalImageButton;
+	wxStaticBitmap *normalImageStatic;
+	
+	wxImageExt maskImage;
+	wxImageExt maskImageDisplay;
+	wxButton *maskImageButton;
+	wxButton *clearMaskButton;
+	wxButton *opacTypeTwo;
+	wxButton *opacTypeThree;
+	wxStaticBitmap *maskImageStatic;
+	
+	wxButton *saveAsButton;
+};
+
+enum
+{
+	BUTTON_NormalImage = wxID_HIGHEST + 1,
+	BUTTON_MaskImage,
+	BUTTON_ClearMask,
+	BUTTON_OpacTypeTwo,
+	BUTTON_OpacTypeThree,
+	BUTTON_SaveAs
+};
+
+BEGIN_EVENT_TABLE(BasicPage, wxNotebookPage)
+
+EVT_BUTTON(BUTTON_NormalImage, BasicPage::OnLoadNormal)
+EVT_BUTTON(BUTTON_MaskImage, BasicPage::OnLoadMask)
+EVT_BUTTON(BUTTON_ClearMask, BasicPage::OnClearMask)
+EVT_BUTTON(BUTTON_OpacTypeTwo, BasicPage::OnOpacTypeTwo)
+EVT_BUTTON(BUTTON_OpacTypeThree, BasicPage::OnOpacTypeThree)
+EVT_BUTTON(BUTTON_SaveAs, BasicPage::OnSaveAs)
+
+END_EVENT_TABLE()
+	
+
+#endif
---
+++ trunk/image_ext.h
@@ -0,0 +1,50 @@
+/*
+ 
+ image_ext.h: extra functionality for wxImage
+ Copyright (C) 2006  Gregory Smith
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ 
+ */
+
+#ifndef IMAGE_EXT_H
+#define IMAGE_EXT_H
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#include <wx/image.h>
+#endif
+
+#define MAX(x, y) (y < x ? x : y)
+
+class wxImageExt : public wxImage
+{
+public:
+	wxImageExt() : wxImage() { }
+	wxImageExt(const wxImageExt& image) : wxImage(image) { }
+	wxImageExt(int width, int height, bool clear=true) : wxImage(width, height, clear) { }
+	
+	void White(); // make the image entirely opaque white
+	void RemoveAlpha(); // remove the alpha channel
+	void MaskToAlpha(); // convert the mask to an alpha channel
+	void PropRescale(int width, int height); // scale to fit but maintain proportions
+	void FromAlpha(const wxImageExt& source); // create a grayscale representation of source's alpha
+	void ToAlpha(wxImageExt& dest) const; // export our grayscale representation to the dest's alpha channel
+	void MakeOpacTypeTwo();
+	void MakeOpacTypeThree();
+};
+
+#endif
